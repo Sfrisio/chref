fd12a0fcb95b7b1dbc8c0650813cb6c96d3aabec
Sfrisio <gianluca.ricci@outlook.com>
2022-11-27 23:06:25 +0100

    update building script

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "chref binary building"
+BRED='\033[1;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-echo "[+] building linux amd64 ..."
-env GOOS=linux GOARCH=amd64 go build -o pkg/linux_amd64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/linux_amd64/. && echo "[+] building linux arm64 ..."
-env GOOS=linux GOARCH=arm64 go build -o pkg/linux_arm64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/linux_arm64/. && echo "[+] building freebsd amd64 ..."
-env GOOS=freebsd GOARCH=amd64 go build -o pkg/freebsd_amd64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/freebsd_amd64/. && echo "[+] building darwin (MacOS) amd64 ..."
+echo -e "\n${BRED}**** chref binary building ****${NC}\n"
+
+echo -e "${GREEN}[+] building linux amd64 ...${NC}"
+env GOOS=linux GOARCH=amd64 go build -o pkg/linux_amd64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/linux_amd64/. && echo -e "${GREEN}[+] building linux arm64 ...${NC}"
+env GOOS=linux GOARCH=arm64 go build -o pkg/linux_arm64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/linux_arm64/. && echo -e "${GREEN}[+] building freebsd amd64 ...${NC}"
+env GOOS=freebsd GOARCH=amd64 go build -o pkg/freebsd_amd64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/freebsd_amd64/. && echo -e "${GREEN}[+] building darwin (MacOS) amd64 ...${NC}"
 env GOOS=darwin GOARCH=amd64 go build -o pkg/darwin_amd64/bin/chref -v -ldflags="-X 'chref/build.Version=$(cat VERSION)' -X 'chref/build.BuildUser=Team chref' -X 'chref/build.BuildTime=$(date)'" && cp -p {VERSION,LICENSE} pkg/darwin_amd64
